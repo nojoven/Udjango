7d89d78f04584b1482e53f24a143ffd735b4aa79
cedricjoseph <work.cedricjoseph@gmail.com>
2020-07-27 17:19:09 +0200

    First template with context

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,10 +1,16 @@
 """These are the views of the food app"""
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = models.Item.objects.all()
+    context = {
+        "item_list": item_list,
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse("<h1>This is an item views</h1>")
