a515bcf5ccb9941bf45b31ec1e8cd65ff89aadb2
cedricjoseph <work.cedricjoseph@gmail.com>
2020-07-29 07:22:59 +0200

    message de succes d'inscription

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,10 +1,18 @@
 """views of users"""
 
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm
-    return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account is created')
+            return redirect('food:index')
+    else:
+        form = UserCreationForm
+        return render(request, 'users/register.html', {'form': form})
