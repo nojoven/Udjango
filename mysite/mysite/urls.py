30cf2168d3e4dc46c3f8a4b1042c878c2f301900
cedricjoseph <work.cedricjoseph@gmail.com>
2020-08-05 09:24:50 +0200

    adding the profile image

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,8 @@ urlpatterns = [
     path('logout/', authentication_views.LogoutView.as_view(template_name='users.logout'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
