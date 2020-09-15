a0b5892acf3ff6d702d8c42053da1d89d9467ebb
Joseph <work.cedricjoseph@gmail.com>
2020-09-16 00:40:59 +0200

    Update settings.py

--- ecom/ecomsite/ecomsite/settings.py
+++ ecom/ecomsite/ecomsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6pv2+&+0c_5yd3@u*t!1otfp^)xtd&d3vb44)eajs#tvvf&bbr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
