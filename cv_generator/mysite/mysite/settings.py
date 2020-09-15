ba69bfa4d0e7dfa78d27e6e500a8ed0b0d7984e3
Joseph <work.cedricjoseph@gmail.com>
2020-09-16 00:40:16 +0200

    Update settings.py

--- cv_generator/mysite/mysite/settings.py
+++ cv_generator/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zzl(_v+b(keh=vfpn1g#qylszh+ky%l5%0y4!@r6228t=2uuq2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
